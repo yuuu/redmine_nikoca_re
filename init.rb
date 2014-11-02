8968d2f84d22432b7b30f1f631bc11a1843adf92
Yuhei Okazaki <yulogbook@gmail.com>
2014-11-02 18:17:38 +0900

    use stylesheet.

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 Redmine::Plugin.register :redmine_nikoca_re do
   name 'Redmine Nikoca Re plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Yuuu'
+  description 'This is a plugin to check every day the mood condition of project members.'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author_url 'http://ylgbk.hatenablog.com/'
 
   project_module :redmine_nikoca_re do
     permission :view_redmine_nikoca_re, :niko_faces => [:index, :show]
