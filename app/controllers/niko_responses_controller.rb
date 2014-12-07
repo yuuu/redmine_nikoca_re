1dcc4ba078d64d752b6f8ed09590558f69f8f785
Yuhei Okazaki <yulogbook@gmail.com>
2014-12-07 16:14:55 +0900

    add documents.

--- app/controllers/niko_responses_controller.rb
+++ app/controllers/niko_responses_controller.rb
@@ -1,13 +1,20 @@
+# Redmine_Nikoca_Re プラグイン NikoResponsesController
+# @author yuuu(Yuhei Okazaki)
+
+# レスポンスの作成・編集・表示などのアクション処理を実装
+# @abstract NikoResponseのアクションに応じた処理を定義
 class NikoResponsesController < ApplicationController
   unloadable
   menu_item :redmine_nikoca_re
   before_filter :find_project
   before_filter :find_niko_face
 
+  # レスポンス一覧を表示する
   def index
     redirect_to project_niko_face_path(@project, @niko_face.id)
   end
 
+  # レスポンスを作成する
   def create
     @niko_response = NikoResponse.new(params[:niko_response])
     @niko_response.author = User.current
@@ -21,12 +28,14 @@ class NikoResponsesController < ApplicationController
   end
 
 private
+  # 現在のプロジェクトを@projectへセットする
   def find_project
     @project = Project.find(params[:project_id])
   rescue ActiveRecord::RecordNotFound
     render_404
   end
 
+  # 現在のニコカレを@niko_faceへセットする
   def find_niko_face
     @niko_face = NikoFace.find(params[:niko_face_id])
   rescue ActiveRecord::RecordNotFound
